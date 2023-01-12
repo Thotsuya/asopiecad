c352ff6d0d43ae98c5ed0acf56a819b7154b5663
Jarliev <55269430+Thotsuya@users.noreply.github.com>
2023-01-12 14:35:00 -0600

    Benefitiaries and goals

--- resources/js/Hooks/Users.js
+++ resources/js/Hooks/Users.js
@@ -1,6 +1,18 @@
 export default function useUsers() {
-    const can = (permission, permissions) => {
-        return permissions.includes(permission)
+    const can = (permissionsToValidate, permissionsArray) => {
+        // If it is an array
+        if (Array.isArray(permissionsToValidate)) {
+            let hasPermission = false
+            permissionsToValidate.forEach((permission) => {
+                if (permissionsArray.includes(permission)) {
+                    hasPermission = true
+                }
+            })
+            return hasPermission
+        }
+
+        // If it is a string
+        return permissionsArray.includes(permissionsToValidate)
     }
 
     return {
